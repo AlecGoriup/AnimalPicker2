8395d326aa885f4d989773d31cf4fdf1315bedd0
AlecGoriup <113146110+AlecGoriup@users.noreply.github.com>
2023-02-28 17:15:56 -0500

    Initial commit to main

---
+++ AnimalPicker2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AnimalPicker2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String animalChoice;
+
+        System.out.println("Which animal would you like to see: a dog or a cat");
+
+        animalChoice = input.nextLine();
+
+        // if (animalChoice.equals("dog")) {
+        // System.out.println(" __ _\no\'\')}____//\n `_/ )\n (_(_/-(_/");
+
+        // } else if (animalChoice.equals("cat")) {
+        // System.out.println(" ^~^ ,\n (\'Y\') )\n / \\/ \n(\\|||/)");
+
+        // }
+
+    }
+}
